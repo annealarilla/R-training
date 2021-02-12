ee2064909508af875d255b10ddb669883669f6a2
Anne Alarilla <anne.alarilla@health.org.uk>
2021-02-12 15:14:19 +0000

    Completing the tutorial including the task

--- Part1/Basic Data Manipulation.R
+++ Part1/Basic Data Manipulation.R
@@ -78,4 +78,143 @@ boxplot(Length ~ Year, data = elongation_long,
         main = "Annual growth of Empetrum hermaphroditum")
 
 #Most common and useful functions of dplyr
+install.packages("dplyr")  # install the package
+library(dplyr)              # load the package
+
+#Rename variables 
+elongation_long <- rename(elongation_long, zone = Zone, indiv = Indiv, year = Year, length = Length)     # changes the names of the columns (getting rid of capital letters) and overwriting our data frame
+
+#Filter and subset columns
+
+elong_subset <- filter(elongation_long, zone %in% c(2, 3), year %in% c("X2009", "X2010", "X2011")) # you can use multiple different conditions separated by commas
+
+elong_no.zone <- dplyr::select(elongation_long, -zone)
+
+# A nice hack! select() lets you rename and reorder columns on the fly
+elong_no.zone <- dplyr::select(elongation_long, Year = year, Shrub.ID = indiv, Growth = length)
+
+#Mutate creates new columns
+
+# CREATE A NEW COLUMN 
+
+elong_total <- mutate(elongation, total.growth = X2007 + X2008 + X2009 + X2010 + X2011 + X2012)
+
+#group_by() certain factors to perform operations on chunks
+
+# GROUP DATA
+
+elong_grouped <- group_by(elongation_long, indiv)   # grouping our dataset by individual
+
+#summarise () data with rance of summary statistics
+
+# SUMMARISING OUR DATA
+
+summary1 <- summarise(elongation_long, total.growth = sum(length))
+summary2 <- summarise(elong_grouped, total.growth = sum(length))
+
+summary3 <- summarise(elong_grouped, total.growth = sum(length),
+                      mean.growth = mean(length),
+                      sd.growth = sd(length))
+#we lose all the other columns in doing this so 
+#may need to merge after this
+
+#join () data sets based on shared attributes
+#there are many types of joins you can perform
+#full_join() will keep everything
+#left_join() keeping everything on the left and only adding what corresponds
+#with the left from the right
+
+
+# Load the treatments associated with each individual
+
+treatments <- read.csv("C:/Users/AnneA/Documents/R projects/R-training/Part1/CC-3-DataManip-master/EmpetrumTreatments.csv", header = TRUE, sep = ";")
+head(treatments)
+
+# Join the two data frames by ID code. The column names are spelled differently, so we need to tell the function which columns represent a match. We have two columns that contain the same information in both datasets: zone and individual ID.
+
+experiment <- left_join(elongation_long, treatments, by = c("indiv" = "Indiv", "zone" = "Zone"))
+
+# We see that the new object has the same length as our first data frame, which is what we want. And the treatments corresponding to each plant have been added!
+
+experiment2 <- merge(elongation_long, treatments, by.x = c("zone", "indiv"), by.y = c("Zone", "Indiv"))  
+# same result!
+
+#Challenge yourself
+
+#load dragons csv
+
+dragons <- read.csv("C:/Users/AnneA/Documents/R projects/R-training/Part1/CC-3-DataManip-master/dragons.csv", header = TRUE, sep = ",")
+head(dragons)
+
+#make the data tidy (long format)
+
+dragons_long<- tidyr::gather(dragons, Spices, Length, c(3:6))
+
+#create a boxplot for each species showing the effect
+#of the spices on plume size
+
+boxplot(Length ~ Spices, data = dragons_long, 
+        xlab = "Spices", ylab = "Plume Length (cm)", 
+        main = "Plume Length by Spices")
+
+#Jalapeno triggered the most fiery reaction
+
+#change the fourth treatment from paprika to turmeric
+
+dragons_long[dragons_long$Spices=="paprika",3]<-"turmeric"
+
+###can also do this
+
+dragons_long <- rename(dragons_long, turmeric = paprika)
+
+
+#There was a calibration error with the measuring device for 
+#the tabasco trial, but only for the Hungarian Horntail species. 
+#All measurements are 30 cm higher than they should be
+
+dragons_long[dragons_long$species=="hungarian_horntail"&& dragons_long$Spices=="tabasco",4]<-dragons_long$Length-30
+
+##can also do this
+# Fix the calibration error for tabasco by horntail
+#but this is before the data is in the long format
+
+
+correct.values  <- dragons$tabasco[dragons$species == 'hungarian_horntail'] - 30   # create a vector of corrected values
+
+dragons[dragons$species == 'hungarian_horntail', 'tabasco'] <- correct.values      # overwrite the values in the dragons object
+
+
+#Converting the Length cm into m
+
+dragons_long$LM<-dragons_long$Length*0.01
+
+#Other way to do it 
+# Convert the data into meters
+
+dragons_long <- mutate(dragons_long, LM= Length/100)    # Creating a new column turning cm into m
+
+#creating the box plots of spices but first
+#filtering by the dragon type to create seperate box plots
+
+d1<-filter(dragons_long, species=="hungarian_horntail")
+d2<-filter(dragons_long, species== "welsh_green")
+d3<- filter(dragons_long, species=="swedish_shortsnout")
+
+# Make the boxplots
+
+par(mfrow=c(1, 3))     
+# you need not have used this, but it splits 
+#3 columns where the plots will appear, so all the plots will be side by side.
+
+boxplot(LM ~ Spices, data = d1,
+        xlab = 'Spice', ylab = 'Length of fire plume (m)',
+        main = 'Hungarian Horntail')
+
+boxplot(LM ~ Spices, data = d2,
+        xlab = 'Spice', ylab = 'Length of fire plume (m)',
+        main = 'Welsh Green')
+
+boxplot(LM ~ Spices, data = d3,
+        xlab = 'Spice', ylab = 'Length of fire plume (m)',
+        main = 'Swedish Shortsnout')
 

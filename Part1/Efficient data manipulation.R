c4952edfae2d008d7ca2a0781eacace4c013d909
Anne Alarilla <anne.alarilla@health.org.uk>
2021-02-18 17:11:34 +0000

    Continuing the training

--- Part1/Efficient data manipulation.R
+++ Part1/Efficient data manipulation.R
@@ -47,7 +47,129 @@ case_when(vector2 == "What am I?" ~ "I am the walrus",
 #you assign new values with ~
 #so we can use this to reassign variables
 
+#changing factor levels or create categorical variables
 
+
+unique(trees$LatinName)  # Shows all the species names
+
+# Create a new column with the tree genera
+
+trees.genus <- trees %>%
+  mutate(Genus = case_when(               # creates the genus column and specifies conditions
+    grepl("Acer", LatinName) ~ "Acer",
+    grepl("Fraxinus", LatinName) ~ "Fraxinus",
+    grepl("Sorbus", LatinName) ~ "Sorbus",
+    grepl("Betula", LatinName) ~ "Betula",
+    grepl("Populus", LatinName) ~ "Populus",
+    grepl("Laburnum", LatinName) ~ "Laburnum",
+    grepl("Aesculus", LatinName) ~ "Aesculus", 
+    grepl("Fagus", LatinName) ~ "Fagus",
+    grepl("Prunus", LatinName) ~ "Prunus",
+    grepl("Pinus", LatinName) ~ "Pinus",
+    grepl("Sambucus", LatinName) ~ "Sambucus",
+    grepl("Crataegus", LatinName) ~ "Crataegus",
+    grepl("Ilex", LatinName) ~ "Ilex",
+    grepl("Quercus", LatinName) ~ "Quercus",
+    grepl("Larix", LatinName) ~ "Larix",
+    grepl("Salix", LatinName) ~ "Salix",
+    grepl("Alnus", LatinName) ~ "Alnus")
+  )
+
+#a quicker way to do this:
+
+library(tidyr)
+trees.genus.2 <- trees %>% 
+  tidyr::separate(LatinName, c("Genus", "Species"), sep = " ", remove = FALSE) %>%  
+  dplyr::select(-Species)
+
+# we're creating two new columns in a 
+#vector (genus name and species name), "sep" refers to the separator, here space between the words, and remove = FALSE means that we want to keep the original column LatinName in the data frame
+
+#another way to reclassify variables
+
+trees.genus <- trees.genus %>%   # overwriting our data frame 
+  mutate(Height.cat =   # creating our new column
+           case_when(Height %in% c("Up to 5 meters", "5 to 10 meters") ~ "Short",
+                     Height %in% c("10 to 15 meters", "15 to 20 meters") ~ "Medium",
+                     Height == "20 to 25 meters" ~ "Tall")
+  )
+
+
+## Reordering a factor's levels
+
+levels(trees.genus$Height.cat)  # shows the different factor levels in their default order
+
+trees.genus$Height.cat <- factor(trees.genus$Height.cat,
+                                 levels = c('Short', 'Medium', 'Tall'),   # whichever order you choose will be reflected in plots etc
+                                 labels = c('SHORT', 'MEDIUM', 'TALL')    # Make sure you match the new names to the original levels!
+)   
+
+levels(trees.genus$Height.cat)  # a new order and new names for the levels
+
+#advanced pipping
+
+# Subset data frame to fewer genera
+
+trees.five <- trees.genus %>%
+  filter(Genus %in% c("Acer", "Fraxinus", "Salix", "Aesculus", "Pinus"))
+
+
+# Map all the trees
+
+(map.all <- ggplot(trees.five) +
+    geom_point(aes(x = Easting, y = Northing, size = Height.cat, colour = Genus), alpha = 0.5) +
+    theme_bw() +
+    theme(panel.grid = element_blank(),
+          axis.text = element_text(size = 12),
+          legend.text = element_text(size = 12))
+)
+
+# Plotting a map for each genus
+
+tree.plots <-  
+  trees.five  %>%      # the data frame
+  group_by(Genus) %>%  # grouping by genus
+  do(plots =           # the plotting call within the do function
+       ggplot(data = .) +
+       geom_point(aes(x = Easting, y = Northing, size = Height.cat), alpha = 0.5) +
+       labs(title = paste("Map of", .$Genus, "at Craigmillar Castle", sep = " ")) +
+       theme_bw() +
+       theme(panel.grid = element_blank(),
+             axis.text = element_text(size = 14),
+             legend.text = element_text(size = 12),
+             plot.title = element_text(hjust = 0.5),
+             legend.position = "bottom")
+  ) 
+
+# You can view the graphs before saving them
+tree.plots$plots
+
+# Saving the plots to file
+
+tree.plots %>%              # the saving call within the do function
+  do(., 
+     ggsave(.$plots, filename = paste(getwd(), "/", "map-", .$Genus, ".png", sep = ""), device = "png", height = 12, width = 16, units = "cm"))
+
+#Task
+View(trees.genus)
+
+#First calculate the quadrants
+lon <- (max(trees.genus$Easting) - min(trees.genus$Easting))/2 + min(trees.genus$Easting)
+lat <- (max(trees.genus$Northing) - min(trees.genus$Northing))/2 + min(trees.genus$Northing)
+
+#create the columns of the different quadrants
+
+trees.genus <- trees.genus %>%
+  mutate(Quadrant = case_when(
+    Easting < lon & Northing > lat ~ 'NW',
+    Easting < lon & Northing < lat ~ 'SW',
+    Easting > lon & Northing > lat ~ 'NE',
+    Easting > lon & Northing < lat ~ 'SE')
+  )
+# We can check that it worked
+ggplot(trees.genus) +
+  geom_point(aes(x = Easting, y = Northing, colour = Quadrant)) +
+  theme_bw()
 
 
 
